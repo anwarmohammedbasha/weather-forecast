cb86748913989461daab75e82f8c1e9051e24e42
anwarmohammedbasha <81125004+anwarmohammedbasha@users.noreply.github.com>
2021-11-17 15:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,17 +3,11 @@ from  statsmodels.tsa.arima.model import ARIMA
 from datetime import date
 import streamlit as st 
 
-
 st.write("""
 # Tamil Nadu Weather Forecast 
-Team Project
-""")
+Team Project""")
 
-city =  st.selectbox('City',
-        ('Ariyalur',  'Chennai',  'Coimbatore',  'Cuddalore',  'Dharmapuri',  'Dindigul',  'Erode',  'Kancheepuram',  'Kanyakumari',  'Karur',  'Krishnagiri',  'Madurai',  'Nagapattinam',  'Namakkal',  'Perambalur',  'Pudukkottai',  'Ramanathapuram',  'Salem',  'Sivaganga',  'Thanjavur',  'Thiruvarur',  'Tiruchirappalli',   'Tirunelveli',  'Tirupur',  'Tiruvallur',  'Tiruvannamalai',  'Vellore',  'Villupuram'))
-
-st.write('You selected:', city)
-
+city =  st.selectbox('City', ('Ariyalur',  'Chennai',  'Coimbatore',  'Cuddalore',  'Dharmapuri',  'Dindigul',  'Erode',  'Kancheepuram',  'Kanyakumari',  'Karur',  'Krishnagiri',  'Madurai',  'Nagapattinam',  'Namakkal',  'Perambalur',  'Pudukkottai',  'Ramanathapuram',  'Salem',  'Sivaganga',  'Thanjavur',  'Thiruvarur',  'Tiruchirappalli',   'Tirunelveli',  'Tirupur',  'Tiruvallur',  'Tiruvannamalai',  'Vellore',  'Villupuram'))
 cols = ['name', 'localtime', 'temp_c']
 df = pd.read_csv('weather_data.csv', usecols=cols, parse_dates=True)
 df = df.loc[df['name'] == city, ['localtime', 'temp_c']]
@@ -22,9 +16,6 @@ df.dropna(inplace=True)
 model = ARIMA(df, order=(5,1,0))
 model_fit = model.fit()
 forecast = int(model_fit.forecast(steps=2)[1:])
-
-
-
 
 if st.button('Predict'):
     today = date.today()
