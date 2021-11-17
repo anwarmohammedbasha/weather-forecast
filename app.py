26ac41dc81911093b1072e3aac03943dd58a9bd0
anwarmohammedbasha <81125004+anwarmohammedbasha@users.noreply.github.com>
2021-11-17 12:58:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ cols = ['name', 'localtime', 'temp_c']
 df = pd.read_csv('weather_data.csv', usecols=cols, parse_dates=True)
 df = df.loc[df['name'] == "Chennai", ['localtime', 'temp_c']]
 df.set_index(['localtime'], inplace=True)
-#df.dropna(inplace=True)
+df.dropna(inplace=True)
 model = ARIMA(df, order=(2,1,1))
 model_fit = model.fit()
 forecast = round(model_fit.forecast(steps=1)[0][0],0)
