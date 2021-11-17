d098365e2d12fa16fc7f1fe9476823b9b7599a82
anwarmohammedbasha <81125004+anwarmohammedbasha@users.noreply.github.com>
2021-11-17 12:52:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,19 +8,19 @@ st.write("""
 still in progress
 """)
 
-def user_input_var():
-    city =  st.selectbox('City',
-             ('Chennai', 'x'))
-    st.write('You selected:', city)
-    cols = ['name', 'localtime', 'temp_c']
-    df = pd.read_csv('weather_data.csv', usecols=cols, parse_dates=True)
-    city_list = list(df.name.unique())
-    df = df.loc[df['name'] == city, ['localtime', 'temp_c']]
-    df.set_index(['localtime'], inplace=True)
-    df.dropna(inplace=True)
-    model = ARIMA(df, order=(5,1,0))
-    model_fit = model.fit()
-    forecast = round(model_fit.forecast(steps=1)[0][0],0)
-    return st.write('date: ', pd.to_datetime("today"), 'weather:', forecast)
+cols = ['name', 'localtime', 'temp_c']
+df = pd.read_csv('weather_data.csv', usecols=cols, parse_dates=True)
+df = df.loc[df['name'] == city, ['localtime', 'temp_c']]
+df.set_index(['localtime'], inplace=True)
+df.dropna(inplace=True)
+model = ARIMA(df, order=(5,1,0))
+model_fit = model.fit()
+forecast = round(model_fit.forecast(steps=1)[0][0],0)
 
+city =  st.selectbox('City',
+        ('Chennai', 'x'))
+st.write('You selected:', city)
 
+if st.button('Predict'):
+    st.write('date: ', pd.to_datetime("today"), 'weather:', forecast)
+else: pass
