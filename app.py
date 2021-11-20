afeaf16b8be1b204cbec1eadaf8e77a6b0a4132b
Anwar <81125004+anwarmohammedbasha@users.noreply.github.com>
2021-11-20 14:56:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,21 +2,10 @@ import pandas as pd
 from  statsmodels.tsa.arima.model import ARIMA
 from datetime import date, timedelta
 import streamlit as st 
-
-st.write("""
-# Tamil Nadu Weather Forecast 
-Team Project""")
-
+st.write("""# Tamil Nadu Weather Forecast""")
 city =  st.selectbox('City', ('Ariyalur',  'Chennai',  'Coimbatore',  'Cuddalore',  'Dharmapuri',  'Dindigul',  'Erode',  'Kancheepuram',  'Kanyakumari',  'Karur',  'Krishnagiri',  'Madurai',  'Nagapattinam',  'Namakkal',  'Perambalur',  'Pudukkottai',  'Ramanathapuram',  'Salem',  'Sivaganga',  'Thanjavur',  'Thiruvarur',  'Tiruchirappalli',   'Tirunelveli',  'Tirupur',  'Tiruvallur',  'Tiruvannamalai',  'Vellore',  'Villupuram'))
-df = pd.read_csv('weather_data.csv', usecols=['name', 'localtime', 'temp_c'], parse_dates=True)
-df = df.loc[df['name'] == city, ['localtime', 'temp_c']]
-df.set_index(['localtime'], inplace=True)
-df.dropna(inplace=True)
+df = pd.read_csv('weather_data.csv', usecols=['name', 'localtime', 'temp_c'], parse_dates=True); df = df.loc[df['name'] == city, ['localtime', 'temp_c']]; df.set_index(['localtime'], inplace=True); df.dropna(inplace=True)
 model = ARIMA(df, order=(5,1,0)).fit()
-#model_fit = model.fit()
 st.write('Todays Weather in ', city ,int(model.forecast(steps=9)[1:2]), '°C')
-
-if st.button('Forecast'):
-    for i in range(1, 7):
-        st.write('Date: ', date.today() + timedelta(i)  , 'Weather: ', int(model.forecast(steps=9)[i+1:i+2]), '°C')
+if st.button('Forecast'): for i in range(1, 7): st.write('Date: ', date.today() + timedelta(i), 'Weather: ', int(model.forecast(steps=9)[i+1:i+2]), '°C')
 else: pass
