e2558c73fe3b2ad0387519742e798a9d3c7634e4
Anwar <81125004+anwarmohammedbasha@users.noreply.github.com>
2021-11-20 14:49:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,9 +14,9 @@ df.set_index(['localtime'], inplace=True)
 df.dropna(inplace=True)
 model = ARIMA(df, order=(5,1,0))
 model_fit = model.fit()
+st.write('Todays Weather in ', city ,int(model_fit.forecast(steps=9)[1:2]), '°C')
 
 if st.button('Forecast'):
-    st.write('Date: ', date.today() , 'Weather: ', int(model_fit.forecast(steps=9)[1:2]), '°C')
     for i in range(1, 7):
         st.write('Date: ', date.today() + timedelta(i)  , 'Weather: ', int(model_fit.forecast(steps=9)[i+1:i+2]), '°C')
 else: pass
