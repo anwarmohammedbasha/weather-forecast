21c69cbba1a91798b8e73280bbcd8181b9c5f741
Anwar <81125004+anwarmohammedbasha@users.noreply.github.com>
2022-01-05 19:12:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,42 +1,19 @@
-# importing essential libraries
-
-#from pyspark.sql import SparkSession
 import pandas as pd
 from  statsmodels.tsa.arima.model import ARIMA
 from datetime import date, timedelta
 import streamlit as st 
 
-#spark = SparkSession.builder.appName('forecast').getOrCreate()
-
 st.write("""# Tamil Nadu Weather Forecast""")
-city =  st.selectbox('City', ('Ariyalur', 'Chennai', 'Coimbatore', 'Cuddalore',
-                              'Dharmapuri', 'Dindigul', 'Erode', 'Kancheepuram',
-                              'Kanyakumari', 'Karur', 'Krishnagiri', 'Madurai',
-                              'Nagapattinam', 'Namakkal', 'Perambalur', 
-                              'Pudukkottai', 'Ramanathapuram', 'Salem', 
-                              'Sivaganga', 'Thanjavur', 'Thiruvarur', 
-                              'Tiruchirappalli', 'Tirunelveli', 'Tirupur',
-                              'Tiruvallur', 'Tiruvannamalai', 'Vellore',
-                              'Villupuram'))
-
-# data preprocessing
-
-#df = spark.read.csv("weatherData.csv", header=True)
-#df = df[df.city == "Chennai"]['date', 'temp_c']
-#df = pd.DataFrame(df.toPandas()).set_index(['date'])
-#df = df['temp_c'].apply(lambda x: int(float(x)))
+city =  st.selectbox('City', ('Ariyalur', 'Chennai', 'Coimbatore', 'Cuddalore', 'Dharmapuri', 'Dindigul', 'Erode', 'Kancheepuram', 'Kanyakumari', 'Karur',
+                              'Krishnagiri', 'Madurai', 'Nagapattinam', 'Namakkal', 'Perambalur', 'Pudukkottai', 'Ramanathapuram', 'Salem', 'Sivaganga',
+                              'Thanjavur', 'Thiruvarur', 'Tiruchirappalli', 'Tirunelveli', 'Tirupur', 'Tiruvallur', 'Tiruvannamalai', 'Vellore', 'Villupuram'))
 
 df = pd.read_csv('weatherData.csv', parse_dates=True)
 df = df.loc[df['city'] == city, ['date', 'temp_c']]
 df.set_index(['date'], inplace=True)
-df.dropna(inplace=True)
-
-# model development
+#df.dropna(inplace=True)
 
 model = ARIMA(df, order=(5,1,0)).fit()
-
-# forecasting
-
 st.write('Todays Weather in ', city ,int(model.forecast(steps=9)[1:2]), '°C')
 
 if st.button('Forecast'): 
