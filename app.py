bf718e8f81df0bbd334903f96d32a63a4b8c55a2
Anwar <81125004+anwarmohammedbasha@users.noreply.github.com>
2022-01-05 19:15:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -11,13 +11,10 @@ city =  st.selectbox('City', ('Ariyalur', 'Chennai', 'Coimbatore', 'Cuddalore', 
 df = pd.read_csv('weatherData.csv', parse_dates=True)
 df = df.loc[df['city'] == city, ['date', 'temp_c']]
 df.set_index(['date'], inplace=True)
-#df.dropna(inplace=True)
-
 model = ARIMA(df, order=(5,1,0)).fit()
-st.write('Todays Weather in ', city ,int(model.forecast(steps=9)[1:2]), '°C')
 
+st.write('Todays Weather in ', city ,int(model.forecast(steps=9)[1:2]), '°C')
 if st.button('Forecast'): 
   for i in range(1, 7):
-    st.write('Date: ', date.today() + timedelta(i),
-             'Weather: ', int(model.forecast(steps=10)[i+1:i+2]), '°C')
+    st.write('Date: ', date.today() + timedelta(i), 'Weather: ', int(model.forecast(steps=10)[i+1:i+2]), '°C')
 else: pass
